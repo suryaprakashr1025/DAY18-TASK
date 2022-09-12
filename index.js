d36cd8ce8fbc6339b4a63c9bba8f0a3341070f6a
suryaprakashr1025 <110890454+suryaprakashr1025@users.noreply.github.com>
2022-09-12 13:33:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ res.then((data)=>data.json())
 .then((value)=>{
  console.log(value);
 
-    for(var i=0; i<3; i++){
-       var div=document.createElement("div")
-        div.style.color="black"
-        div.style.fontSize="32px";
-        div.style.margin="50px"
-        div.innerHTML=`
-        <div class="container" style="border:1px solid black; padding:10px 10px; background-color:grey">
-        <div class="row">
-        <div class="col-lg-4" >
+var container=document.createElement("div");
+container.setAttribute("class","container")
+var row=document.createElement("div");
+row.classList.add("row","m-3");
+container.append(row);
+
+    for(var i=0; i<value.length; i++){
+    
+        row.innerHTML+=`
+        <div class="col-lg-4">
 
         <div class="card border-success mb-3" style="max-width: 600px; background-color:white: height:500px">
         <div class="card-header" style="text-align:center">${value[i].name}</div>
@@ -20,74 +21,12 @@ res.then((data)=>data.json())
         <h5 class="card-title" style="color:black">CAPITAL: ${value[i].capital}</h5>
         <h5 class="card-title" style="color:black">REGION: ${value[i].region}</h5>
         <h5 class="card-title" style="color:black">COUNTRY CODE: ${value[i].alpha3Code}</h5>
-        <a class="btn btn-primary" href="https://openweathermap.org/find?q=${value[i].name}" role="button"style="marigin-bottom:50px">Click for weather</a>
-
-        </div>
-        </div>
-        </div>
-        <div class="col-lg-4">
-
-        <div class="card border-success mb-3" style="max-width: 600px; background-color:white">
-        <div class="card-header" style="text-align:center">${value[i=i+1].name}</div>
-        <div class="card-body text-success" style="text-align:center">
-        <h5 class="card-title" style="text-align:center"><img src="${value[i].flags.svg}" style="width:300px"></h5>
-        <h5 class="card-title" style="color:black">CAPITAL: ${value[i].capital}</h5>
-        <h5 class="card-title" style="color:black">REGION: ${value[i].region}</h5>
-        <h5 class="card-title" style="color:black">COUNTRY CODE: ${value[i].alpha3Code}</h5>
-        <a class="btn btn-primary" href="https://openweathermap.org/find?q=${value[i].name}" role="button">Click for weather</a>
-
-        </div>
-        </div>
-        </div>
-        <div class="col-lg-4" style="height:200px">
-
-        <div class="card border-success mb-3" style="max-width: 600px; background-color:white">
-        <div class="card-header" style="text-align:center">${value[i=i+1].name}</div>
-        <div class="card-body text-success" style="text-align:center">
-        <h5 class="card-title"><img src="${value[i].flags.svg}" style="width:275px"></h5>
-        <h5 class="card-title" style="color:black">CAPITAL: ${value[i].capital}</h5>
-        <h5 class="card-title" style="color:black">REGION: ${value[i].region}</h5>
-        <h5 class="card-title" style="color:black">COUNTRY CODE: ${value[i].alpha3Code}</h5>
-        <a class="btn btn-primary" href="https://openweathermap.org/find?q=${value[i].name}" role="button">Click for weather</a>
-
-        </div>
-        </div>
-        </div>
+        <a class="btn btn-primary" href="https://openweathermap.org/find?utf8=%E2%9C%93&q=${value[i].name}" role="button"style="marigin-bottom:50px">Click for weather</a>
+        
         </div>
         </div>
       `
-        document.body.append(div);
+        document.body.append(container);
     }
 });
-
-
-//var res=fetch("https://data.covid19india.org/v4/min/data.min.json")
-//res.then((data)=>data.json())
-//.then((value)=>{
-   // console.log(value);
-   // bar(value);
-//});
-
-//function bar(value){
-//for(var i in value){
-    //console.log(i,value[i])
-      
-      // div.style.color="green"
-      // div.style.fontSize="32px";
-      //if(i==="TN"){
-      //var div=document.createElement("div")
-      // div.innerHTML=`
-      //<div class="card border-success mb-3" style="max-width: 18rem;">
-      //  <div class="card-header">${i}</div>
-       // <div class="card-body text-success">
-      //  <h5 class="card-title">${value[i].total.confirmed}</h5>
-      //  <h5 class="card-title">${value[i].total.deceased}</h5>
-      //  </div>
-    //  </div>
-    //  `
-  //    document.body.append(div);
-  //  }}
-  
-//}
-
     
